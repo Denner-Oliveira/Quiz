47faa2fb8e77dceb5a1377f1e00151655664e26b
unknown <denneroliveira322@gmail.com>
2023-01-14 14:13:12 -0300

    finalizando

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ let numQuestão = document.querySelector('#question-number')
 let valorQuestao = Number(document.querySelector('#question-number').textContent)
 let textoQuestao = document.querySelector('#question-text')
 let botoes = [...document.querySelectorAll('#answers-box button')]
+let final = document.querySelector('#resultado')
+let txtAcertos = document.querySelector('#txtAcertos')
+let porAcertos = document.querySelector('#porAcertos')
+let usuario = document.querySelector('#paraUsuario')
 
 let perguntas = [
     {
@@ -71,68 +75,78 @@ let perguntas = [
 ]
 
 let num;
+let rodada;
+let acertos = 0
 
 function init(){
   num = 0
+  rodada = num;
   questaoDaVez(num);
 }
 
 function questaoDaVez(rodada){
   numQuestão.textContent++
-  textoQuestao.textContent = perguntas[rodada].question
-  respostas(rodada);
+  textoQuestao.textContent = perguntas[rodada].question;
+  botoes.forEach(element=>{ 
+    let indice = botoes.indexOf(element);
+    element.childNodes[2].textContent = perguntas[rodada].answers[indice].answer
+  })
+  respostas();
 }
 
-function respostas(rodada){
-  for(i=0;i<botoes.length;i++){
-    botoes[i].childNodes[2].textContent = perguntas[rodada].answers[i].answer
-  }
-  verificaResposta(rodada)
+function respostas(){
+  botoes.forEach(a=> a.addEventListener('click',verificaResposta))
 }
 
-function verificaResposta(rodada){
-  for(i=0;i<botoes.length;i++){
-    botoes[i].addEventListener('click',function a(){click(this)})
+function verificaResposta(){  
+  let indice = botoes.indexOf(this);
+  if(perguntas[rodada].answers[indice].correct == true){
+    botoes.forEach(element => element.classList.add('wrong-answer'))
+    botoes[indice].classList.add('correct-answer')
+    rodada++;
+    setTimeout(()=>{
+      botoes.forEach(element=> element.classList.remove('wrong-answer','correct-answer'))
+      if(rodada == 3){
+        acertos++
+        resultado()
+      }else{
+        acertos++
+        questaoDaVez(rodada)
+      } 
+    },800)
+  }else{
+    botoes.forEach(element => element.classList.add('wrong-answer'))
+    rodada++;
+    setTimeout(()=>{
+      botoes.forEach(element=> element.classList.remove('wrong-answer'))
+      if(rodada == 3){
+        resultado()
+      }else{
+        questaoDaVez(rodada)
+      }
+    },800)
   }
-} 
+}
 
 function resultado(){
-  quizz.classList = 'hide'
+  quizz.classList.add('hide');
+  final.classList.remove('hide');
+  verificaAcertos(acertos)
+
 }
 
-function click(x){
-  x.removeEventListener('click',function a(){click(this)})
-  let indice = botoes.indexOf(x)
-  //botoes.forEach(a => a.classList = 'wrong-answer');
-  //botoes[indice].classList = 'correct-answer';
-  if(num > 2){
-    resultado();      
-  }else{      
-    //botoes.forEach(a=> a.classList.remove('correct-answer','wrong-answer'));    
-    //num++;      )
-    questaoDaVez(num);
+function verificaAcertos(x){
+  let por = x * 100
+  let result = por / perguntas.length
+  
+  if(x >= 2){
+    usuario.textContent = 'PARABÉNS!'
+  }else{
+    usuario.textContent = 'Podemos melhorar!'
   }
-          /*
-            x.classList = 'wrong-answer';
-           s if(num > 2){
-              resultado();
-            }else{
-            x.classList.remove('wrong-answer');
-            num++;
-            //questaoDaVez(num);
-          */
-          }
+  txtAcertos.textContent = `De ${perguntas.length} perguntas você acertou ${x}`
+  porAcertos.textContent = `${result.toFixed(2)}%`
+
+}
 
 init()
-
-
-
-
-/*
-if(perguntas[rodada].answers[indice].correct == true){
-    console.log('if')
-
-else{
-  console.log('else')
-}
-*/
